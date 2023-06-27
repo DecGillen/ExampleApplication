91d9a4c68bb874e417a8e40017d32baacd456264
D Gillen <declangillen@live.com>
2023-06-27 22:00:00 +0100

    added continuous delivery

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo apt update && sudo apt install nodejs npm
 
 sudo npm install -g pm2
@@ -6,6 +8,6 @@ pm2 stop example_app
 
 cd ExampleApplication/
 
-npm install 
+npm install
 
 pm2 start ./bin/www --name example_app
